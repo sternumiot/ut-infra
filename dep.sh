eb226b74a4e3bf63a9e56b5d6347287fe0d9081d
Lian Granot <lian@sternumiot.com>
2022-05-08 09:08:33 +0300

    improved install

--- dep.sh
+++ dep.sh
@@ -1,3 +1,5 @@
-apt-get install valgrind
-apt-get install python
-apt-get install gcov
+sudo apt install valgrind
+sudo apt install python-is-python3
+sudo apt install make
+sudo apt install gcc
+sudo apt install gcov
